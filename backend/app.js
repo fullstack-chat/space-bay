bf6864238b4bfe7f92cb63c872d44e2559e3e620
DMDXV <Padfootdiamond@Gmail.com>
2020-11-07 16:16:42 +0000

    Winston logging added

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv')
+const morgan = require('morgan');
+const winston = require('./config/winston');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+dotenv.config()
+
+app.use(morgan('combined', { stream: winston.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+app.listen(process.env.PORT, () => {
+    console.log(`Space Bay API available at: http://localhost:${process.env.PORT}`)
+})
